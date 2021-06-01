736cab500ff16fcfa8d5f73f61af7f52d881ffaa
gim <jjqian@creams-cli>
2021-06-01 16:09:01 +0800

    feat: nodemon 重新启动

--- src/service/tenant/indexService.ts
+++ src/service/tenant/indexService.ts
@@ -1,6 +1,6 @@
 import BaseService from '../index';
 import { handleClass, handleResponse } from '@/utils/request';
-// @handleClass(__dirname) // 包裹方法的错误处理方式
+@handleClass(__dirname) // 包裹方法的错误处理方式
 class IndexService extends BaseService {
   constructor() {
     super()
